c580cd395dbdea9478f960c6f3bdfb2d93d2b3d4
rohanailoni <rohanailoni@gmail.com>
2022-11-01 23:12:14 +0530

    added custome problemId not found exception

--- Execptions/Arguments.py
+++ Execptions/Arguments.py
@@ -15,7 +15,14 @@ class fileargnotfound(Exception):
         return "The submit argument is required as contestId is mentioned"
 
     
-    
+class problemIdargnotfound(Exception):
+    def __init__(self,*args):
+        super().__init__(args)
+    def __str__(self):
+        return "The problem Id is mandetory to submit a contest problem"
+
+
+
 
 
 
