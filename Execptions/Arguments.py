8e483f358e40d24ba01ae5cd2f149f1217a4f8e2
rohanailoni <rohanailoni@gmail.com>
2022-11-01 23:33:33 +0530

    added new custome excetion in argument.py where it handles the wrong problem id is given

--- Execptions/Arguments.py
+++ Execptions/Arguments.py
@@ -1,6 +1,13 @@
 
 # this page is sepcially dealting with custom exceptions for errors in arguments
+class wrongproblemId(Exception):
+    def __init__(self,id,def_list,*args):
+        super().__init__(args)
+        self.id=id
+        self.def_list=def_list
 
+    def __str__(self):
+        return "The problem id {self.id} is not in the default list of ID's that are {self.def_list}"
 
 
 class submitargnotfound(Exception):
@@ -8,6 +15,8 @@ class submitargnotfound(Exception):
         super().__init__(args)
     def __str__(self):
         return "The submit argument is required as contestId is mentioned"
+
+
 class fileargnotfound(Exception):
     def __init__(self,*args):
         super().__init__(args)
@@ -20,6 +29,7 @@ class problemIdargnotfound(Exception):
         super().__init__(args)
     def __str__(self):
         return "The problem Id is mandetory to submit a contest problem"
+
 
 
 
