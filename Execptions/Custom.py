fd91ce10ba8b26dfdcf85684c915f47e7e5bd182
rohanailoni <rohanailoni@gmail.com>
2022-11-02 00:09:56 +0530

    added the custom function which check weather there is a session or not

---
+++ Execptions/Custom.py
@@ -0,0 +1,8 @@
+# This page is for creating custom errors 
+
+
+class SessionNotFound(Exception):
+    def __init__(self,*args):
+        super().__init__(args)
+    def __str__(self):
+        return "The session file is not found Try login in again"
